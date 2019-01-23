9aa9b1c84396479e1621cc34f12991dd0bf5afb2
Alexis <46349373+AlexisIsCoding@users.noreply.github.com>
2019-01-23 13:13:43 +0100

    [Update] Adding comments to the Script.js file
    Structuration et ajout de commentaires au fichier Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,11 @@
+// ------------------------------- Fonctions | Functions -------------------------------
+
+// ********** Animation de fondu d'opacité **********
+// ********** Opacity Fade Animation **********
 function opacityFade() {
 
-    if (way === "increase") {
-
+    if (way === "increase") { // Montée d'opacité jusqu'à 1 | Opacity's up to 1
+        
         if (landmarkOpacity < 1) {
 
             slideshowElmt.style.opacity = landmarkOpacity + speed;
@@ -10,15 +14,14 @@ function opacityFade() {
 
             requestAnimationFrame(opacityFade);
 
-        } else {
+        } else { // Opacité = 1 : Changement de sens - Fin de l'animation | Opacity = 1 : Direction Change - Animation end
 
             way = "decrease";
 
             cancelAnimationFrame(animationFade);
         }
 
-
-    } else if (way === "decrease") {
+    } else if (way === "decrease") { // Descente d'opacité jusqu'à 0 | Opacity's down to 0
 
         if (landmarkOpacity > 0) {
 
@@ -28,7 +31,7 @@ function opacityFade() {
 
             requestAnimationFrame(opacityFade);
 
-        } else {
+        } else { // Remise à zéro de l'Opacité : Changement de sens - Fin de l'animation | Opacity's set to 0 : Direction Change - Animation end
 
             landmarkOpacity = 0;
 
@@ -41,57 +44,87 @@ function opacityFade() {
 
     }
 
-    // console.log(slideshowElmt.style.opacity);
 }
 
+
+
+// ********** Fonction de roulement du diaporama (Chargement initial de la page) **********
+// ********** "Onload" Slideshow rolling function (Initial case) **********
 function slideshowRolling() {
 
+    // Délai de la montée d'opacité | Opacity rise Timeout
     firstTimeOut = setTimeout(opacityFade, 50);
+    
+    // Délai de la descente d'opacité | Opacity descent Timeout
     secondTimeOut = setTimeout(opacityFade, 9000);
 
+    // Définition d'une valeur aléatoire | Setting a random value
     randomValue = Math.floor(Math.random() * picturesList.length);
     // console.log(randomValue);
 
+    // Changement d'image parmis un élément aléatoire de la liste de photo | Image change from random element in the pictures list
     slideshowElmt.style.backgroundImage = picturesList[randomValue];
 }
 
-function slideshowReverse() {
 
+
+// ********** Fonction de roulement du diaporama (en cas de changement d'onglet) **********
+// ********** "Onblur/Onfocus" Slideshow rolling function ("Changing tab" case) **********
+function slideshowReverse() {
+    
+    // Délai de la descente d'opacité | Opacity descent Timeout
     firstTimeOut = setTimeout(opacityFade, 50);
 
-    optionalTimeOut = setTimeout(function () {
-
+    optionalTimeOut = setTimeout(function () { // Délai du changement aléatoire de l'image | Picture random change's Timeoout
+        
+        // Définition d'une valeur aléatoire | Setting a random value
         randomValue = Math.floor(Math.random() * picturesList.length);
-        // console.log(randomValue);
 
+        // Changement d'image parmis un élément aléatoire de la liste de photo | Image change from random element in the pictures list
         slideshowElmt.style.backgroundImage = picturesList[randomValue];
-        // console.log(slideshowElmt.style.backgroundImage);
     }, 1050);
 
+    // Délai de la montée d'opacité | Opacity rise Timeout
     secondTimeOut = setTimeout(opacityFade, 1100);
 }
 
-var slideshowElmt = document.getElementById("slideshow");
-// var diapoImg = getComputedStyle(slideshowElmt).backgroundImage;
-// var diapoOpacity = getComputedStyle(slideshowElmt).opacity;
 
+
+// ------------------------------- Variables communes | Commons variables -------------------------------
+
+// Élément contenant le diaporama | Element containing the slide show 
+var slideshowElmt = document.getElementById("slideshow");
+
+// Liste d'images | Pictures list
 var picturesList = ["url(\"https://cdn.pixabay.com/photo/2016/02/19/11/51/louvre-1210004_1280.jpg\")", "url(\"https://cdn.pixabay.com/photo/2016/03/09/09/45/paris-1245970_1280.jpg\")"];
+
+// Variable de la valeur aléatoire | Random value variable
 var randomValue = undefined;
 
-var animationFade = 0;
+// Variables des valeurs de direction, de repère et de vitesse du changement d'opacité | Variables of direction, landmark and speed values of the opacity change
 var way = "increase";
 var landmarkOpacity = 0;
 var speed = 0.02;
 
+var animationFade = 0;
+
+// Variables des différents délais utilisés | Timeout used's variables 
 var firstTimeOut = undefined;
 var secondTimeOut = undefined;
 var optionalTimeOut = undefined;
 var intervalRolling = setInterval(slideshowRolling, 10000);
 
+// ------------------------------- Initialisation | Initailization -------------------------------
+
+// ********** Initailisation du diaporama initial au chargement de la page **********
+// ********** Initailization of the initial slideshowRolling when page is on load **********
 slideshowRolling();
 intervalRolling;
 
-window.onfocus = function () {
+
+// ********** Initailisation du diaporama inversé à la reprise du focus sur l'onglet **********
+// ********** Initailization of the slideshowReverse when the focus is reopened on the tab **********
+window.onfocus = function () { 
 
     way = "decrease";
     
@@ -101,6 +134,8 @@ window.onfocus = function () {
 };
 
 
+// ********** Maintien de l'image et de l'opacité & réinitialisation des délais et des fonctions à la perte du focus (Changement d'onglet) **********
+// ********** Maintain image and opacity & reset timeout and functions at focus loss (Tab change) **********
 window.onblur = function () {
 
     way = "increase";
@@ -108,6 +143,7 @@ window.onblur = function () {
     landmarkOpacity = 1;
     slideshowElmt.style.opacity = landmarkOpacity;
 
+    // Réinitialisation des délais et des fonctions | Reset timeout and functions
     clearInterval(intervalRolling);
     clearTimeout(optionalTimeOut);
     clearTimeout(secondTimeOut);
